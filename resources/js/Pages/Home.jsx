814c7d522ec8cb0f21dbbcea794bc6432183327f
earcra0106 <haji1003@outlook.jp>
2025-04-06 09:42:22 +0900

    フロントエンド練習

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,17 +1,42 @@
 import React from "react";
+import { Box, Heading, VStack, HStack, Image } from "@chakra-ui/react";
 
 const Home = (props) => {
     return (
         <>
-            <h1>メモ帳内容</h1>
-            <ul>
-                {props.notepads.map((notepad) => (
-                    <li key={notepad.id}>
-                        <p>{notepad.name}</p>
-                        <p>{notepad.created_at}</p>
-                    </li>
-                ))}
-            </ul>
+            <Box mt={2} bg="tomato">
+                <Heading
+                    as="h1"
+                    size="lg"
+                    mb={4}
+                    fontSize={{ base: "24px", md: "40px", lg: "56px" }}
+                >
+                    Home
+                </Heading>
+                <VStack spacing={4} align="stretch">
+                    {props.notepads.map((notepad) => (
+                        <Box key={notepad.id} p={4}>
+                            <HStack spacing={4}>
+                                <Image
+                                    boxSize="100px"
+                                    objectfit="cover"
+                                    src="https://via.placeholder.com/100"
+                                    alt="Notepad Image"
+                                />
+                            </HStack>
+                        </Box>
+                    ))}
+                </VStack>
+                <h1>メモ帳内容</h1>
+                {/* <ul>
+                    {props.notepads.map((notepad) => (
+                        <li key={notepad.id}>
+                            <p>{notepad.name}</p>
+                            <p>{notepad.created_at}</p>
+                        </li>
+                    ))}
+                </ul> */}
+            </Box>
         </>
     );
 };
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('home', [NotepadController::class, 'index'])->name('notepad.index');
+Route::get('/home', [NotepadController::class, 'index'])->name('notepad.index');
 
 require __DIR__.'/auth.php';
