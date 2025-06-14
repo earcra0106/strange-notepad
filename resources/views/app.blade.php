af234f04118ad40f61f5b561627545c2f40bab4a
earcra0106 <haji1003@outlook.jp>
2025-06-15 02:24:33 +0900

    HTTPのMixed解消

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="{{ asset('strange-notepad-favicon.png') }}">
+        <link rel="icon" href="{{ secure_asset('strange-notepad-favicon.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
